1bb7eeae26225fba07214592ace3e942999ad70f
JR Gestao <admin@jrgestao.app>
2026-08-27 14:29:17 -0300

    Revert "v40.14.5 dialog de observacoes como caixa central"
    This reverts commit 4a34e38d81d108626ad2c55e7833574c2f2b198e.

--- src/services-lazy-v29.js
+++ src/services-lazy-v29.js
@@ -53,7 +53,7 @@ function loadBoardV25(replay = true) {
 
     loaded.board =
       import(
-        './services-board-v28.js?v=v40-14-5-20260827-dialog-box'
+        './services-board-v28.js?v=v40-14-4-20260827-obs-square'
       )
         .then((module) => {
           const first =
